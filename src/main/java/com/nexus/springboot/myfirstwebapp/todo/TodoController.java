8bea58c05e4bcff621cd4b469e2b4f741c23a342
Juan Pablo <juanpasan2000@gmail.com>
2024-05-20 10:50:03 -0500

    Delete Todo feature

--- src/main/java/com/nexus/springboot/myfirstwebapp/todo/TodoController.java
+++ src/main/java/com/nexus/springboot/myfirstwebapp/todo/TodoController.java
@@ -48,4 +48,10 @@ public class TodoController {
         return "redirect:list-todos";
     }
 
+    @RequestMapping(value = "delete-todo", method = RequestMethod.GET)
+    public String deleteTodo(@RequestParam int id) {
+        todoService.deleteById(id);
+        return "redirect:list-todos";
+    }
+
 }
